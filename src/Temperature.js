70a7c0a1342175e98d1f7457189d221673dc6958
adumas555 <91496550+adumas555@users.noreply.github.com>
2022-01-29 21:36:29 -0500

    Integrated unit conversion to current weather

--- src/Temperature.js
+++ src/Temperature.js
@@ -3,13 +3,43 @@ import React, { useState } from "react";
 export default function Temperature(props) {
   let [unit, setUnit] = useState("fahrenheit");
 
+  function fToC(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function cToF(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function celsius() {
+    return ((props.fahrenheit - 32) * 5) / 9;
+  }
+
   if (unit === "fahrenheit") {
     return (
       <div className="Temperature">
         <span className="temperature">{Math.round(props.fahrenheit)}</span>
-        <span className="unit">°F | <a href="/">°C</a> </span>
+        <span className="unit">
+          °F |{" "}
+          <a href="/" className="text-decoration-none" onClick={fToC}>
+            °C
+          </a>{" "}
+        </span>
+      </div>
     );
-  } else{
-
+  } else {
+    return (
+      <div className="Temperature">
+        <span className="temperature">{Math.round(celsius())} </span>
+        <span className="unit">
+          <a href="/" className="text-decoration-none" onClick={cToF}>
+            °F
+          </a>{" "}
+          | °C{" "}
+        </span>
+      </div>
+    );
   }
 }
